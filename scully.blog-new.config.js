1110c03265b5490e82eca927d0f2c5555096960a
thomascsd <thomascsd808@gmail.com>
2020-04-11 15:55:37 +0800

    add scully

---
+++ scully.blog-new.config.js
@@ -0,0 +1,13 @@
+exports.config = {
+  projectRoot: "./src",
+  projectName: "blog-new",
+  outDir: './dist/static',
+  routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
+  }
+};
