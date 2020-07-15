948f248e9f9b2c7bd5c323202811b1fac89a97cc
thomascsd <thomascsd808@gmail.com>
2020-07-15 22:15:16 +0800

    add comments

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,28 +1,41 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject, AfterViewInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 import { ScullyRoutesService, ScullyRoute } from '@scullyio/ng-lib';
 import { BlogService } from '../core/blog.service';
-
-declare var ng: any;
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css'],
   preserveWhitespaces: true,
-  encapsulation: ViewEncapsulation.Emulated,
 })
 export class BlogComponent implements OnInit {
-  title = '';
-  bgImageUrl = '';
-  publishedDate = '';
   post: ScullyRoute;
 
-  constructor(private scullyService: ScullyRoutesService, private blogService: BlogService) {}
+  constructor(
+    private scullyService: ScullyRoutesService,
+    private blogService: BlogService,
+    private titleService: Title,
+    private renderer2: Renderer2,
+    @Inject(DOCUMENT) private document
+  ) {}
 
   ngOnInit() {
     this.scullyService.getCurrent().subscribe((route) => {
       this.post = route;
       this.post.date = this.blogService.getPostDateFormRoute(this.post.route);
+      this.titleService.setTitle(route.title);
     });
+
+    const s = this.renderer2.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://utteranc.es/client.js';
+    s.setAttribute('repo', 'thomascsd/thomascsd.github.io');
+    s.setAttribute('issue-term', 'pathname');
+    s.setAttribute('theme', 'github-light');
+    s.setAttribute('crossorigin', 'anonymous');
+    s.text = ``;
+    this.renderer2.appendChild(this.document.querySelector('#comments'), s);
   }
 }
