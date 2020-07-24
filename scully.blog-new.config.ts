d9066f90614951cf28f0ac59b0e63401333e449a
thomascsd <thomascsd808@gmail.com>
2020-07-25 00:43:10 +0800

    add sitemap

--- scully.blog-new.config.ts
+++ scully.blog-new.config.ts
@@ -1,6 +1,24 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
 
 setPluginConfig('md', { enableSyntaxHighlighting: true });
+// const SitemapPlugin = getSitemapPlugin();
+/*setPluginConfig(SitemapPlugin, {
+  urlPrefix: 'https://thomascsd.github.io/',
+  sitemapFilename: 'sitemap.xml',
+  merge: false,
+  changeFreq: 'monthly',
+  priority: ['1.0', '0.9', '0.8', '0.7', '0.6', '0.5', '0.4', '0.3', '0.2', '0.1', '0.0'],
+  ignoredRoutes: ['/404'],
+  routes: {
+    '/blog/:slug': {
+      changeFreq: 'daily',
+      priority: '0.9',
+      sitemapFilename: 'sitemap-blogs.xml',
+      merge: true,
+    },
+  },
+});*/
 
 export const config: ScullyConfig = {
   projectRoot: './src',
