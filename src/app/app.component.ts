f2608ee065ae8e6a296ed908c55f946643e8847d
thomascsd <thomascsd808@gmail.com>
2020-07-25 10:43:23 +0800

    add GA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { distinctUntilChanged } from 'rxjs/operators';
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'blog-new';
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        distinctUntilChanged((previous: any, current: any) => {
+          if (current instanceof NavigationEnd) {
+            return previous.url === current.url;
+          }
+          return true;
+        })
+      )
+      .subscribe((x: any) => {
+        gtag('event', 'page_view', { page_path: x.url });
+      });
+  }
+}
 }
