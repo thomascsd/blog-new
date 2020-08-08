974632ba11bb48ae0381975b8d281bac603ad965
thomascsd <thomascsd808@gmail.com>
2020-08-08 21:13:49 +0800

    fix GA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,33 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { distinctUntilChanged } from 'rxjs/operators';
 declare let gtag: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {
-    this.router.events
-      .pipe(
-        distinctUntilChanged((previous: any, current: any) => {
-          if (current instanceof NavigationEnd) {
-            return previous.url === current.url;
-          }
-          return true;
-        })
-      )
-      .subscribe((x: any) => {
-        gtag('event', 'page_view', { page_path: x.url });
-      });
+    // this.router.events
+    //   .pipe(
+    //     distinctUntilChanged((previous: any, current: any) => {
+    //       if (current instanceof NavigationEnd) {
+    //         return previous.url === current.url;
+    //       }
+    //       return true;
+    //     })
+    //   )
+    //   .subscribe((x: any) => {
+    //     gtag('event', 'page_view', { page_path: x.url });
+    //   });
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('event', 'page_view', event.url);
+      }
+    });
   }
 }
-
