16329bc25bbdb9b0a480c5c8e873be4fa9ba38fb
Thomas Chang <thomascsd808@gmail.com>
2021-04-25 21:42:50 +0800

    v11

--- src/app/shared/post-header/post-header.component.spec.ts
+++ src/app/shared/post-header/post-header.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PostHeaderComponent } from './post-header.component';
 
@@ -6,7 +6,7 @@ describe('PostHeaderComponent', () => {
   let component: PostHeaderComponent;
   let fixture: ComponentFixture<PostHeaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PostHeaderComponent ]
     })
--- src/app/shared/skill-tree/skill-tree.component.spec.ts
+++ src/app/shared/skill-tree/skill-tree.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SkillTreeComponent } from './skill-tree.component';
 
@@ -6,7 +6,7 @@ describe('SkillTreeComponent', () => {
   let component: SkillTreeComponent;
   let fixture: ComponentFixture<SkillTreeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SkillTreeComponent ]
     })
